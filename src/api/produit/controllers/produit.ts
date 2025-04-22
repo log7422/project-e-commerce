93dfb5e42653148b4c5e8e606556eafce36e9de9
Logan <eonologan@gmail.com>
2025-04-22 14:07:08 +0200

    add post method

--- src/api/produit/controllers/produit.ts
+++ src/api/produit/controllers/produit.ts
@@ -1,7 +1,47 @@
 import axios from 'axios';
-import { factories } from '@strapi/strapi';
+import {factories} from '@strapi/strapi';
 
 export default factories.createCoreController('api::produit.produit', ({ strapi }) => ({
+
+    async create(ctx) {
+        const body = ctx.request.body;
+
+        try {
+            const response = await axios.post(
+                'https://api.airtable.com/v0/appBEXBO3G9kJ2bga/Produits',
+                {
+                    fields: {
+                        "nom": body.nom,
+                        "quantite": body.quantite,
+                        "prix": body.prix,
+                        "In Promo": body["In Promo"],
+                        "date d'ajout": body["date d'ajout"],
+                        "description": body.description,
+                        "Actual Price": body["Actual Price"],
+                        "Discounted Price": body["Discounted Price"],
+                        "image": body.image,
+                    },
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer patXEDvOirByjd3aT.14a47a9e80520f93529731d4f81af26b7fbca0c2b7263ddef4ff32e0f3345697`,
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+
+            return {
+                message: 'Produit ajouté avec succès dans Airtable ✅',
+                record: response.data,
+            };
+        } catch (error) {
+            strapi.log.error('Erreur POST Airtable:', error.message);
+            ctx.throw(500, 'Impossible d’ajouter le produit dans Airtable');
+        }
+    },
+
+
+
     async find(ctx) {
         const query = ctx.query;
         const filters = [];
